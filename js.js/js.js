6e2c37488979471407ce8c6ff6dc615788c4e26e
agusvlopez <agusvlopez@gmail.com>
2023-04-10 21:43:59 -0300

    Cambio de imagenes

--- js.js/js.js
+++ js.js/js.js
@@ -3,10 +3,10 @@ const app = new Vue ({
     data: {
         cardHover: false,
         imagenes: [
-            {href: "https://agusvlopez.github.io/Blur---maquillajes/" , src: "file:///C:/Users/sioma/Documents/GitHub/Portafolio/img/proyectos/blur_maquillajes1.png" , titulo: "Sitio Web de Maquillajes", sub: "Html Css Bootstrap Responsive", enable: true},
-            {href: "https://agusvlopez.github.io/nintendo/" , src: "file:///C:/Users/sioma/Documents/GitHub/Portafolio/img/proyectos/nintendo1.png" , titulo: "One Page de Nintendo", sub: "Html Css Responsive", enable: true},
-            {href: "https://agustina783405.invisionapp.com/console/share/Q4B5UNHY9KW/977181096" , src: "file:///C:/Users/sioma/Documents/GitHub/Portafolio/img/proyectos/yoga.png" , titulo: "Prototipo Ecommerce de cursos de Yoga", sub: "Prototipo Wireframing Adobe Illustrator UX/UI", enable: true},
-            {href: "https://agusvlopez.github.io/disk-list/" , src: "file:///C:/Users/sioma/Documents/GitHub/Portafolio/img/proyectos/list-disk.png" , titulo: "Lista de discos con Js", sub: "Javascript Html Css", enable: true},
+            {href: "blur_maquillajes.png" , src: "file:///C:/Users/sioma/Documents/GitHub/Portafolio/img/proyectos/blur_maquillajes1.png" , titulo: "Sitio Web de Maquillajes", sub: "Html Css Bootstrap Responsive", enable: true},
+            {href: "nintendo.png" , src: "file:///C:/Users/sioma/Documents/GitHub/Portafolio/img/proyectos/nintendo1.png" , titulo: "One Page de Nintendo", sub: "Html Css Responsive", enable: true},
+            {href: "yoga.png" , src: "file:///C:/Users/sioma/Documents/GitHub/Portafolio/img/proyectos/yoga.png" , titulo: "Prototipo Ecommerce de cursos de Yoga", sub: "Prototipo Wireframing Adobe Illustrator UX/UI", enable: true},
+            {href: "disk-list.png" , src: "file:///C:/Users/sioma/Documents/GitHub/Portafolio/img/proyectos/list-disk.png" , titulo: "Lista de discos con Js", sub: "Javascript Html Css", enable: true},
         ],
     },
     
